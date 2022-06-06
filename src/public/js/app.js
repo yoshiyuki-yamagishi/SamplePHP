42a1ee043f0d0988f4c190180a86e855db51ebf3
LB-YutaNomura <yuta.nomura@linkedbrain.jp>
2022-06-06 18:48:48 +0900

    JS追加　ホバー

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
+//画面遷移のスクリプト
+
+$(window).on('load',function(){
+    $("#splash-logo").delay(1200).fadeOut('slow');
+
+
+    $("#splash").delay(1500).fadeOut('slow',function(){
+
+        $('body').addClass('appear');
+
+    });
+
+    $('.splashbg').on('animationend', function() {
+    });
+
+});
+
 
